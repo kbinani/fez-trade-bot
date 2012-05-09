af65f27e6b4799b2e5c5a05b3d95f906317fb4d5
kbinani <kbinani@msn.com>
2012-05-10 02:20:29 +0900

    modify RuntimeSettings confファイルのparse処理をリファクタ

--- RuntimeSettings.cs
+++ RuntimeSettings.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 
 namespace FEZTradeBot {
-    class RuntimeSettings {
+    public class RuntimeSettings {
         private string logDirectory = "";
         private string fezLauncher = "";
         private string loginId = "";
@@ -27,63 +28,27 @@ namespace FEZTradeBot {
                     if( 2 <= parameters.Length ) {
                         string key = parameters[0];
                         string value = parameters[1];
-                        switch( key ) {
-                            case "logDirectory": {
-                                logDirectory = value;
-                                break;
-                            }
-                            case "fezLauncher": {
-                                fezLauncher = value;
-                                break;
-                            }
-                            case "loginId": {
-                                loginId = value;
-                                break;
-                            }
-                            case "loginPassword": {
-                                loginPassword = value;
-                                break;
-                            }
-                            case "loginCharacterName": {
-                                loginCharacterName = value;
-                                break;
-                            }
-                            case "tellMessage.inventoryNoSpace": {
-                                tellMessageInventoryNoSpace.Add( value );
-                                break;
-                            }
-                            case "tellMessage.succeeded": {
-                                tellMessageSucceeded.Add( value );
-                                break;
-                            }
-                            case "tellMessage.weiredItemEntried": {
-                                tellMessageWeiredItemEntried.Add( value );
-                                break;
-                            }
-                            case "adminPC": {
-                                adminPC = value;
-                                break;
-                            }
-                            case "irc.host": {
-                                ircHost = value;
-                                break;
-                            }
-                            case "irc.port": {
-                                ircPort = int.Parse( value );
-                                break;
-                            }
-                            case "irc.password": {
-                                ircPassword = value;
-                                break;
-                            }
-                            case "irc.channelName": {
-                                ircChannelName = value;
-                                break;
-                            }
+                        string fieldName = GetFieldName( key );
+                        var type = this.GetType();
+                        var fieldInfo = type.GetField( fieldName, BindingFlags.Instance | BindingFlags.NonPublic );
+                        if( fieldInfo == null ) {
+                            continue;
+                        }
+                        if( fieldInfo.FieldType == typeof( List<string> ) ) {
+                            var list = (List<string>)fieldInfo.GetValue( this );
+                            list.Add( value );
+                            fieldInfo.SetValue( this, list );
+                        } else if( fieldInfo.FieldType == typeof( int ) ) {
+                            fieldInfo.SetValue( this, int.Parse( value ) );
+                        } else if( fieldInfo.FieldType == typeof( string ) ) {
+                            fieldInfo.SetValue( this, value );
                         }
                     }
                 }
             }
+        }
+
+        protected RuntimeSettings() {
         }
 
         public string LogDirectory {
@@ -175,6 +140,27 @@ namespace FEZTradeBot {
             }
             int index = random.Next( messageCandidates.Count - 1 );
             return messageCandidates[index];
+        }
+
+        /// <summary>
+        /// confファイルのキー名を、このクラスのフィールド名に変換する
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        protected string GetFieldName( string key ) {
+            var parameters = key.Split( '.' );
+            for( int i = 1; i < parameters.Length; i++ ) {
+                var parameter = parameters[i];
+                var c = new string( parameter[0], 1 ).ToUpper();
+                parameter = c + parameter.Substring( 1 );
+                parameters[i] = parameter;
+            }
+
+            var result = "";
+            foreach( var parameter in parameters ) {
+                result += parameter;
+            }
+            return result;
         }
     }
 }
---
+++ test/RuntimeSettingsTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using FEZTradeBot;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace FEZTradeBotTest {
+    class RuntimeSettingStub : RuntimeSettings {
+        public new string GetFieldName( string name ) {
+            return base.GetFieldName( name );
+        }
+    }
+
+    class RuntimeSettingsTest {
+        [TestCase]
+        public static void GetFieldName() {
+            var setting = new RuntimeSettingStub();
+            var actual = setting.GetFieldName( "foo.barBaz.helloWorld" );
+            Assert.AreEqual( "fooBarBazHelloWorld", actual );
+        }
+    }
+}
