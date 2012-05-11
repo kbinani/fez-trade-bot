2599af5b9a42d49b7bbfed2fe85aeec18d009749
kbinani <kbinani@msn.com>
2012-05-11 20:30:00 +0900

    add PixelEnumerator

---
+++ test/PixelEnumeratorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NUnit.Framework;
+using FEZTradeBot;
+
+namespace FEZTradeBotTest {
+    class PixelEnumeratorTest {
+        [TestCase]
+        public static void Enumerate() {
+            int width = 3;
+            int height = 2;
+            var pixels = PixelEnumerator.GetEnumerable( width, height ).GetEnumerator();
+            for( int y = 0; y < height; y++ ) {
+                for( int x = 0; x < width; x++ ) {
+                    pixels.MoveNext();
+                    Assert.AreEqual( new Point( x, y ), pixels.Current );
+                }
+            }
+            Assert.False( pixels.MoveNext() );
+        }
+    }
+}
