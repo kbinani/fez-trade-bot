efa69063e834acc80e0b8380a07c9fe6e4f7c0eb
kbinani <kbinani@msn.com>
2012-05-24 23:24:00 +0900

    modify 取引ログの保存に失敗した際の処理を追加

--- TradeLog.cs
+++ TradeLog.cs
@@ -6,20 +6,14 @@ namespace FEZTradeBot {
     class TradeLog {
         const string DATABASE = "fez-trade-bot";
         private static RuntimeSettings settings;
-        private static MySqlConnection connection;
         private static Encoding encoding;
 
         public static void Init( RuntimeSettings settings ) {
             TradeLog.settings = settings;
             encoding = new UTF8Encoding( false );
 
-            var config = string.Format(
-                "server={0};user id={1}; password={2}; database=" + DATABASE + "; pooling=false",
-                settings.SqlHost, settings.SqlUser, settings.SqlPassword );
-            connection = new MySqlConnection( config );
-            connection.Open();
-
-            var command = new MySqlCommand( @"
+            using( var connection = CreateConnection() ) {
+                var command = new MySqlCommand( @"
 CREATE TABLE IF NOT EXISTS `trade_log` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) NOT NULL,
@@ -30,29 +24,52 @@ CREATE TABLE IF NOT EXISTS `trade_log` (
   KEY `idx_time` (`time`),
   KEY `idx_status` (`status`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8", connection );
-            command.ExecuteNonQuery();
+                command.ExecuteNonQuery();
+            }
         }
 
         public static void Insert( string name, DateTime time, TradeResult.StatusType status ) {
             var sql = "insert into trade_log ( name, time, status ) values( @name, @time, @status )";
-            var command = new MySqlCommand( sql, connection );
-            command.Parameters.AddWithValue( "name", name );
-            command.Parameters.AddWithValue( "time", time );
-            command.Parameters.AddWithValue( "status", status.ToString() );
-            command.ExecuteNonQuery();
+            MySqlConnection connection = null;
+            try {
+                connection = CreateConnection();
+                var command = new MySqlCommand( sql, connection );
+                command.Parameters.AddWithValue( "name", name );
+                command.Parameters.AddWithValue( "time", time );
+                command.Parameters.AddWithValue( "status", status.ToString() );
+                command.ExecuteNonQuery();
+            } catch( Exception e ) {
+                var message = "取引ログを記録できなかった。name=" + name + "; time=" + time + "; status=" + status.ToString();
+                Console.Error.WriteLine( message );
+                Irc.SendMessage( message );
+            } finally {
+                if( connection != null ) {
+                    connection.Dispose();
+                }
+            }
         }
 
         public static DateTime GetLastTradeTime( string name ) {
             var sql = "select time from trade_log where name = @name and status = @status order by time desc limit 1";
-            var command = new MySqlCommand( sql, connection );
-            command.Parameters.AddWithValue( "name", name );
-            command.Parameters.AddWithValue( "status", TradeResult.StatusType.SUCCEEDED.ToString() );
-            var reader = command.ExecuteReader();
-            if( reader.Read() ) {
-                return (DateTime)reader.GetValue( 0 );
-            } else {
-                return new DateTime( 1900, 1, 1 );
+            using( var connection = CreateConnection() ) {
+                var command = new MySqlCommand( sql, connection );
+                command.Parameters.AddWithValue( "name", name );
+                command.Parameters.AddWithValue( "status", TradeResult.StatusType.SUCCEEDED.ToString() );
+                var reader = command.ExecuteReader();
+                if( reader.Read() ) {
+                    return (DateTime)reader.GetValue( 0 );
+                }
             }
+            return new DateTime( 1900, 1, 1 );
+        }
+
+        private static MySqlConnection CreateConnection() {
+            var config = string.Format(
+                "server={0};user id={1}; password={2}; database=" + DATABASE + "; pooling=false",
+                settings.SqlHost, settings.SqlUser, settings.SqlPassword );
+            var connection = new MySqlConnection( config );
+            connection.Open();
+            return connection;
         }
     }
 }
