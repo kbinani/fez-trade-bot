a26ff2c42e7ed9bda69b033c1d3ae0414b0099f6
kbinani <kbinani@msn.com>
2012-06-10 16:21:42 +0900

    add project, clear_virtual_keyboard_state; デバッグ時に仮想キーボードが押しっぱなしになることがあるので、これをリセットするツールを追加した

---
+++ clear_virtual_keyboard_state/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FEZTradeBot {
+    class Program {
+        static void Main( string[] args ) {
+            using( var client = new VMultiKeyboardClient() ) {
+                client.ClearKey();
+            }
+        }
+    }
+}
